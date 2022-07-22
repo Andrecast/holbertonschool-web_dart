12e305f4351829d3621abf46e4a7d97a3bc8525f
andrea <acastrillon@ec.krugercorp.com>
2022-07-22 08:57:51 -0500

    Another files

--- 0x02-dart-classes/5-json_to_user.dart
+++ 0x02-dart-classes/5-json_to_user.dart
@@ -1,31 +1,27 @@
 class User {
-  int id = 0;
-  String name = "";
+  User(
+      {required this.id,
+      required this.name,
+      required this.age,
+      required this.height});
+  String name;
+  int id;
   int age;
   double height;
 
-  User({required this.id, required this.name, required this.age, required this.height});
+  Map toJson() {
+    return {"id": id, "name": name, "age": age, "height": height};
+  }
 
   static User fromJson(Map<dynamic, dynamic> userJson) {
     return User(
-      id: userJson['id'],
-      name: userJson['name'],
-      age: userJson['age'],
-      height: userJson['height'],
-    );
+        id: userJson['id'],
+        name: userJson['name'],
+        age: userJson['age'],
+        height: userJson['height']);
   }
 
-  Map<String, dynamic> toJson() {
-    return {
-      'id': id,
-      'name': name,
-      'age': age,
-      'height': height,
-    };
-  }
-
-  @override
   String toString() {
-    return 'User(id : $id ,name: $name,  age: $age, height: $height)';
+    return "User(id : $id ,name: $name, age: $age, height: $height)";
   }
 }
